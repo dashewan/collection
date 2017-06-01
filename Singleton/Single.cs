36fc16a3166ace40852964aebb94e1998a683d6d
dashewan <wang1787326278@163.com>
2017-06-01 18:09:52 +0800

    添加单例模式

---
+++ Singleton/Single.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+   public class Single
+    {
+        private static Single single;
+        private Single()
+        {
+           
+        }
+        private static object obj=new object();
+        public static Single getInstance()
+        {
+            lock (obj) {
+                if (single == null)
+                {
+                    lock (obj) {
+                        single = new Single();
+                    }
+                }
+            }
+            return single;
+        }
+    }
+}
